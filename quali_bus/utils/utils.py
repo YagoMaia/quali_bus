9301cec0f098bacce1dc667f11f98a615f320fa7
yagomaia <yagohenriquev123@gmail.com>
2025-05-20 16:54:25 -0300

    refactor: melhora a estrutura e eficiência do código
    - Refatora a classe Associador para melhorar a legibilidade e eficiência
    - Remove funções não utilizadas e simplifica cálculos de distância
    - Adiciona validações e tratamento de erros
    - Cria nova classe GeradorCores para melhor organização
    - Remove arquivos não utilizados (execptions.py e utils.py)

--- quali_bus/utils/utils.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-
-def converter_para_datetime(df, coluna, formato):
-	"""Converte uma coluna para o formato datetime especificado."""
-	df[coluna] = pd.to_datetime(df[coluna], format=formato)
-	return df
